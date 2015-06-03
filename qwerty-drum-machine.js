28ba7a00118b236af4175756fcb5c142c5e3267e
Jason A. Storey <jason.storey@unrulymedia.com>
2015-06-03 14:43:00 +0100

    Render a crude UI

--- qwerty-drum-machine.js
+++ qwerty-drum-machine.js
@@ -30,26 +30,58 @@ function startUserMedia(stream) {
 }
 
 function initQDM(recorder) {
-    var trigger = new Trigger({
-        keyCode:65,
-        recorder: recorder
-    });
+    var triggers = [],
+        KEY_CODES = [65, 83, 68, 70, 71, 72, 74, 75, 76];
 
-    trigger.init();
-    trigger.draw();
+    for(var i = 0; i < KEY_CODES.length; i++) {
+        triggers.push(new Trigger({
+            keyCode: KEY_CODES[i],
+            recorder: recorder,
+            label: 'Trigger #' + i
+        }));
+
+        triggers[i].init();
+        triggers[i].draw();
+    }
+
+    window.addEventListener('keydown', checkKeyPressed, false);
+    window.addEventListener('keyup', checkKeyReleased, false);
+
+    function checkKeyPressed(e) {
+        triggers.forEach(function(trigger) {
+            if (e.keyCode == trigger.keyCode) {
+                trigger.startSource();
+            }
+        });
+    }
+
+    function checkKeyReleased(e) {
+        triggers.forEach(function(trigger) {
+            if (e.keyCode == trigger.keyCode) {
+                trigger.stopSource();
+            }
+        });
+    }
 }
 
 function Trigger(options) {
     this.keyCode = options.keyCode;
     this.recorder = options.recorder;
+    this.playing = false;
+    this.label = options.label;
 }
 
 Trigger.prototype = {
     init: function() {
         this.container = document.createElement('div');
+        this.display = document.createElement('div');
+        this.keyCodeContainer = document.createElement('h3');
         this.startButton = document.createElement('button');
         this.stopButton = document.createElement('button');
-        this.display = document.createElement('ul');
+        this.labelContainer = document.createElement('h3');
+
+        this.display.innerHTML = 'No sample';
+        this.keyCodeContainer.innerHTML = String.fromCharCode(this.keyCode);
 
         this.startButton.innerHTML = 'record';
         this.startButton.addEventListener('click', function() {
@@ -57,15 +89,20 @@ Trigger.prototype = {
         }.bind(this));
 
         this.stopButton.innerHTML = 'stop';
-        this.stopButton.setAttribute('disabled', true);
-
+        this.stopButton.disabled = true;
         this.stopButton.addEventListener('click', function() {
             this.stopRecording();
         }.bind(this));
 
+        this.labelContainer.innerHTML = this.label;
+
         this.container.appendChild(this.display);
+        this.container.appendChild(this.keyCodeContainer);
         this.container.appendChild(this.startButton);
         this.container.appendChild(this.stopButton);
+        this.container.appendChild(this.labelContainer);
+
+        this.container.className = 'trigger';
     },
     draw: function() {
         document.body.appendChild(this.container);
@@ -80,28 +117,50 @@ Trigger.prototype = {
         this.recorder.stop();
         this.stopButton.disabled = true;
         this.startButton.disabled = false;
-        while (this.display.hasChildNodes()) {
-            this.display.removeChild(this.display.lastChild);
-        }
+        this.display.innerHTML = 'Loading Sample';
+
         console.log('Stopped recording.');
 
-        // create WAV download link using audio data blob
-        this.recorder.exportWAV(function(blob) {
-            var url = URL.createObjectURL(blob);
-            var li = document.createElement('li');
-            var au = document.createElement('audio');
-            var hf = document.createElement('a');
-
-            au.controls = true;
-            au.src = url;
-            hf.href = url;
-            hf.download = new Date().toISOString() + '.wav';
-            hf.innerHTML = hf.download;
-            li.appendChild(au);
-            li.appendChild(hf);
-            this.display.appendChild(li);
+        this.recorder.getBuffer(function(buffer) {
+            this.buffer = getAudioBufferFromData(2, buffer);
+            this.display.innerHTML = 'Sample Loaded';
         }.bind(this));
 
         this.recorder.clear();
+    },
+    startSource: function() {
+        if(this.playing || !this.buffer) {
+            return;
+        }
+        this.source = audioContext.createBufferSource();
+        this.source.buffer = this.buffer;
+        this.source.connect(audioContext.destination);
+        this.source.start(0);
+        this.playing = true;
+        this.container.classList.add('playing');
+    },
+    stopSource: function() {
+        if(!this.playing) {
+            return;
+        }
+        this.source.stop();
+        this.playing = false;
+        this.container.classList.remove('playing');
     }
 };
+
+function getAudioBufferFromData(channels, data) {
+    var frameCount = data[0].length,
+        myArrayBuffer = audioContext.createBuffer(channels, frameCount, audioContext.sampleRate),
+        channel,
+        nowBuffering;
+
+    for (channel = 0; channel < channels; channel++) {
+        nowBuffering = myArrayBuffer.getChannelData(channel);
+        for (var i = 0; i < frameCount; i++) {
+            nowBuffering[i] = data[channel][i];
+        }
+    }
+
+    return myArrayBuffer;
+}
