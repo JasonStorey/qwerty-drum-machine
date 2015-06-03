b412e8fca23b1554e486fa207d28a886162061ca
Jason A. Storey <jason.storey@unrulymedia.com>
2015-06-03 12:33:11 +0100

    Refactor into Trigger component

--- qwerty-drum-machine.js
+++ qwerty-drum-machine.js
@@ -1,70 +1,17 @@
-var audio_context;
+var audioContext;
 var recorder;
-
-var startButton = document.createElement('button');
-var stopButton = document.createElement('button');
 var recordingslist = document.createElement('ul');
 
-startButton.innerHTML = 'record';
-startButton.addEventListener('click', function(event) {
-    startRecording(event.target);
-});
-
-stopButton.innerHTML = 'stop';
-stopButton.setAttribute('disabled', true);
-
-stopButton.addEventListener('click', function(event) {
-    stopRecording(event.target);
-});
-
-document.body.appendChild(startButton);
-document.body.appendChild(stopButton);
 document.body.appendChild(recordingslist);
 
 function startUserMedia(stream) {
-    var input = audio_context.createMediaStreamSource(stream);
+    var input = audioContext.createMediaStreamSource(stream);
     console.log('Media stream created.');
-    //input.connect(audio_context.destination);
+    //input.connect(audioContext.destination);
 
     recorder = new Recorder(input);
     console.log('Recorder initialised.');
-}
-
-function startRecording(button) {
-    recorder && recorder.record();
-    button.disabled = true;
-    stopButton.disabled = false;
-    console.log('Recording...');
-}
-
-function stopRecording(button) {
-    recorder && recorder.stop();
-    button.disabled = true;
-    startButton.disabled = false;
-    console.log('Stopped recording.');
-
-    // create WAV download link using audio data blob
-    createDownloadLink();
-
-    recorder.clear();
-}
-
-function createDownloadLink() {
-    recorder && recorder.exportWAV(function(blob) {
-        var url = URL.createObjectURL(blob);
-        var li = document.createElement('li');
-        var au = document.createElement('audio');
-        var hf = document.createElement('a');
-
-        au.controls = true;
-        au.src = url;
-        hf.href = url;
-        hf.download = new Date().toISOString() + '.wav';
-        hf.innerHTML = hf.download;
-        li.appendChild(au);
-        li.appendChild(hf);
-        recordingslist.appendChild(li);
-    });
+    init();
 }
 
 window.onload = function init() {
@@ -74,7 +21,7 @@ window.onload = function init() {
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
         window.URL = window.URL || window.webkitURL;
 
-        audio_context = new AudioContext;
+        audioContext = new AudioContext;
         console.log('Audio context set up.');
         console.log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
     } catch (e) {
@@ -85,3 +32,80 @@ window.onload = function init() {
         console.log('No live audio input: ' + e);
     });
 };
+
+function Trigger(options) {
+    this.keyCode = options.keyCode;
+    this.recorder = options.recorder;
+}
+
+Trigger.prototype = {
+    init: function() {
+        this.container = document.createElement('div');
+        this.startButton = document.createElement('button');
+        this.stopButton = document.createElement('button');
+        this.display = document.createElement('ul');
+
+        this.startButton.innerHTML = 'record';
+        this.startButton.addEventListener('click', function() {
+            this.startRecording();
+        }.bind(this));
+
+        this.stopButton.innerHTML = 'stop';
+        this.stopButton.setAttribute('disabled', true);
+
+        this.stopButton.addEventListener('click', function() {
+            this.stopRecording();
+        }.bind(this));
+
+        this.container.appendChild(this.display);
+        this.container.appendChild(this.startButton);
+        this.container.appendChild(this.stopButton);
+    },
+    draw: function() {
+        document.body.appendChild(this.container);
+    },
+    startRecording: function() {
+        this.recorder.record();
+        this.startButton.disabled = true;
+        this.stopButton.disabled = false;
+        console.log('Recording...');
+    },
+    stopRecording: function() {
+        this.recorder.stop();
+        this.stopButton.disabled = true;
+        this.startButton.disabled = false;
+        while (this.display.hasChildNodes()) {
+            this.display.removeChild(this.display.lastChild);
+        }
+        console.log('Stopped recording.');
+
+        // create WAV download link using audio data blob
+        this.recorder.exportWAV(function(blob) {
+            var url = URL.createObjectURL(blob);
+            var li = document.createElement('li');
+            var au = document.createElement('audio');
+            var hf = document.createElement('a');
+
+            au.controls = true;
+            au.src = url;
+            hf.href = url;
+            hf.download = new Date().toISOString() + '.wav';
+            hf.innerHTML = hf.download;
+            li.appendChild(au);
+            li.appendChild(hf);
+            this.display.appendChild(li);
+        }.bind(this));
+
+        this.recorder.clear();
+    }
+};
+
+function init() {
+    var trigger = new Trigger({
+        keyCode:65,
+        recorder: recorder
+    });
+
+    trigger.init();
+    trigger.draw();
+}
