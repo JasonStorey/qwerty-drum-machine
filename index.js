353b4460e0b6d86b51bd8a71ca4137ab4e4de8bd
Priyanka Patel <priyanka.patel@unrulygroup.com>
2015-06-16 00:45:44 +0100

    Refactor setup logic into index.js and move drum machine controller into separate module

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = require('./lib/main.js');
+var audioUtils = require('./lib/audio-utils.js'),
+    QDM = require('./lib/qwerty-drum-machine.js');
+
+var Recorder,
+    audioContext,
+    triggersContainer,
+    controlsContainer;
+
+function audioStreamHandler(stream) {
+    var input = audioContext.createMediaStreamSource(stream),
+        recorder = new Recorder(input);
+
+    QDM.init({
+        recorder: recorder,
+        triggersContainer: triggersContainer,
+        controlsContainer: controlsContainer
+    });
+}
+
+module.exports = {
+    init: function(options) {
+        Recorder = options.recorder;
+        audioContext = audioUtils.getAudioContext();
+        audioUtils.getAudioStream(audioStreamHandler);
+        triggersContainer = options.triggersContainer;
+        controlsContainer = options.controlsContainer;
+    }
+};
